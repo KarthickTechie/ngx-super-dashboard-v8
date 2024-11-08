809f02ac4436766b486944fb2997c3a5f1651428
lathamaniTech <lathamani.srinivasarao@sysarcinfomatix.com>
2024-11-08 16:57:12 +0530

    angular 8 dashboard library
    reusable dashboard library developed to support angular 8 projects.

---
+++ projects/ngx-super-dashboard/src/lib/ngx-super-dashboard.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { NgxSuperDashboardComponent } from "./ngx-super-dashboard.component";
+import { GoogleChartsModule } from "angular-google-charts";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [NgxSuperDashboardComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, GoogleChartsModule],
+  exports: [NgxSuperDashboardComponent],
+})
+export class NgxSuperDashboardModule {}
